9b22afaddf78ef040b75c54848bce9a7444d095f
SANDLINI <dominictorreto870@gmial.com>
2023-11-01 07:50:10 +0530

    Add svg to mobile header

--- src/components/Header/MobileHeader.jsx
+++ src/components/Header/MobileHeader.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import image from '../../assets/mobile.png';
-import styles from './MobileHeader.module.css';
+import React from "react";
+import image from "../../assets/mobile.png";
+import image2 from "../../assets/lines.png";
+import styles from "./MobileHeader.module.css";
 const MobileHeader = () => {
   return (
     <section>
       <div className={styles.main}>
         <div>
-          <img src={image} alt="Header Image"  className= {styles.image}/>
+          <img src={image} alt="Header Image" className={styles.image} />
         </div>
-        <div className= {styles.content}>
+        <div className={styles.content}>
           <h4 className={styles.heading}>Ace Our Single Shot Challenge !</h4>
+          <span>
+            <img src={image2} className= {styles.image2}/>
+          </span>
           <p className={styles.header_para}>
             Jumpstart Your Career with Our Free Assessment Test: Prove Your
             Skills and Secure an Internship Today!
           </p>
-        <button className= {styles.button}>Take A Test</button>
+          <button className={styles.button}>Take A Test</button>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default MobileHeader
+export default MobileHeader;
